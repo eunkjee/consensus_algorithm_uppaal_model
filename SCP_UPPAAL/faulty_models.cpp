765a260e5bf1694f8617dc383bc7c9f1b2b08bbc
Youlim Jayde Jung <yljung.jay@gmail.com>
2025-07-01 08:43:22 +0900

    added: FM7 not voting on a first round node

--- SCP_UPPAAL/faulty_models.cpp
+++ SCP_UPPAAL/faulty_models.cpp
@@ -1,5 +1,5 @@
 // processMessageNomination -- blocking threshold check for accept nomination
-        /*
+        /* FM1
             FAULTY ACTION: REJECT 'v' against the blocking set's decision
             (original: .accepted[v] = true;)
         */
@@ -7,13 +7,11 @@
             cntValues > (getTotalValidators(myID) - qs[slotCur][myID].threshold) // blocking threshold
             && !nominationStates[myID].accepted[v]
         ) {
-            
-            
             nominationStates[myID].accepted[v] = false; 
             nominationStates[myID].voted[v] = false;
         }
 
-        /*
+        /* FM2
             FAULTY ACTION: ACCEPT 'v' below blocking threshold
             (original: if (cntValues > (...)){ })
         */
@@ -21,14 +19,12 @@
             cntValues <= (getTotalValidators(myID) - qs[slotCur][myID].threshold) // blocking threshold
             && !nominationStates[myID].accepted[v]
         ) {
-            
-            
             nominationStates[myID].accepted[v] = true; 
             nominationStates[myID].voted[v] = false;
         }
 
 // processMessageNomination -- quorum threshold check for vote nomination
-        /*
+        /* FM3
             FAULTY ACTION: REJECT 'v' despite exceeding threshold 
             (original: accepted[v] = true;)
         */
@@ -38,18 +34,18 @@
             nominationStates[myID].voted[v] = false;
         }
 
-        /*
+        /* FM4
             FAULTY ACTION: ACCEPT 'v' below threshold
             (original: if (cntValues >= (...)) { })
         */
-       if (cntValues < qs[slotCur][myID].threshold
+        if (cntValues < qs[slotCur][myID].threshold
             && !nominationStates[myID].accepted[v]) {
             nominationStates[myID].accepted[v] = true;
             nominationStates[myID].voted[v] = false;
         }
 
 // processMessageNomination -- quorum threshold check for accept nomination
-        /*
+        /* FM5
             FAULTY ACTION: REJECT 'v' despite exceeding threshold 
             (original: accepted[v] = true;)
         */
@@ -67,11 +63,11 @@
             }
         }
 
-        /*
+        /* FM6
             FAULTY ACTION: ACCEPT 'v' below threshold
             (original: if (cntValues >= (...)) { })
         */
-       if (cntValues < qs[slotCur][myID].threshold) {
+        if (cntValues < qs[slotCur][myID].threshold) {
             if (!nominationStates[myID].accepted[v]) {
                 nominationStates[myID].accepted[v] = true;
                 nominationStates[myID].voted[v] = false;
@@ -84,3 +80,15 @@
                 }
             }
         }
+
+// newRound -- proceeding the first vote
+                /* FM7
+                    FAULTY ACTION: NOT VOTE FOR LEADER'S DECISION
+                    (original: else { })
+                */
+                // get a leader
+                nodeIDHighest = getHighestPriorityNode(cur_round[myID]);
+                if (nodeIDHighest == myID) {
+                    // if leader is self, get and vote for a higher layer input.
+                    getMyHigherlayerInput();
+                }
